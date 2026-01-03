c004e16108d16589fd0c1542c9abf0f379feb65b
“y.ikeda” <y.ikeda@nagatake.co.jp>
2026-01-03 17:03:51 +0900

    springboot学習　バインド追加

--- SpringBootSample/src/main/java/jp/co/nagatake/controller/SignupController.java
+++ SpringBootSample/src/main/java/jp/co/nagatake/controller/SignupController.java
@@ -7,14 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.nagatake.application.service.UserApplicationService;
+import jp.co.nagatake.form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
 	@Autowired
@@ -22,7 +26,7 @@ public class SignupController {
 	
 	/** ユーザ登録画面表示 */
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale) {
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
 		/** 性別を取得 */
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
@@ -32,7 +36,9 @@ public class SignupController {
 	
 	/** ユーザ登録処理 */
 	@PostMapping("/signup")
-	public String postSignup() {
+	public String postSignup(@ModelAttribute SignupForm form) {
+		
+		log.info(form.toString());
 		// ログイン画面へリダイレクト
 		return "redirect:/login";
 	}
