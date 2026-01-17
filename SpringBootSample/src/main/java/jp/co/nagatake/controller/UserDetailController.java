4de42b170f4dfdd27b3cfe8b1a30a10a3ee7aba5
“y.ikeda” <y.ikeda@nagatake.co.jp>
2026-01-17 21:35:47 +0900

    springboot学習　mybatis実装（ユーザー詳細画面実装）

---
+++ SpringBootSample/src/main/java/jp/co/nagatake/controller/UserDetailController.java
@@ -0,0 +1,44 @@
+package jp.co.nagatake.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.nagatake.domain.user.model.MUser;
+import jp.co.nagatake.domain.user.service.UserService;
+import jp.co.nagatake.form.UserDetailForm;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserDetailController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	/* ユーザー詳細画面を表示 */
+	@GetMapping("/detail/{userId:.+}")
+	public String getUser(UserDetailForm form, Model model, @PathVariable("userId") String userId) {
+		
+		// ユーザーを1件取得
+		MUser user = userService.getUserOne(userId);
+		user.setPassword(null);
+		
+		// MUserをformに変換
+		form = modelMapper.map(user, UserDetailForm.class);
+		
+		// modelに登録
+		model.addAttribute("userDetailForm", form);
+		
+		return "user/detail";
+	}
+	
+
+}
